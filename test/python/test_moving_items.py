c191d909f9191d450304b6a781b49c9130311c75
Wilfried <wifried.mercier.pro@pm.me>
2026-08-28 18:56:41 +0200

    Started adding integration tests for the python API

---
+++ test/python/test_moving_items.py
@@ -0,0 +1,154 @@
+import dash
+from   dash.testing.composite                  import DashComposite
+from   selenium.webdriver.common.action_chains import ActionChains
+from   selenium.webdriver.common.by            import By
+
+from   .fixtures import (
+    app_with_two_items,
+    app_with_locked_items
+)
+
+def test_moving_unlocked_item_without_handle(dash_duo: DashComposite, app_with_two_items: dash.Dash) -> None:
+    '''Check that a SortableItem without a handle can be dragged around.'''
+
+    dash_duo.start_server(app_with_two_items)
+    actions = ActionChains(dash_duo.driver)
+
+    source = dash_duo.find_element('component2', attribute='ID')
+    target = dash_duo.find_element('component1', attribute='ID')
+
+    initial_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    # Click and drag + release -> it should move the source to the top and move the target down
+    actions.click_and_hold(source)
+    actions.pause(0.5)
+    actions.move_to_element(target)
+    actions.pause(0.5)
+    actions.release().perform()
+
+    final_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    # Check that positions have changed
+    assert initial_locations != final_locations, 'Dragging and dropping component2 failed.'
+    assert final_locations['source']['y'] < initial_locations['source']['y'], 'source did not move vertically'
+    assert final_locations['target']['y'] > initial_locations['target']['y'], 'target did not move vertically'
+    assert final_locations['source']['y'] < final_locations['target']['y'], 'source is not above target after drag and drop'
+
+    return
+
+def test_moving_unlocked_item_with_handle(dash_duo: DashComposite, app_with_two_items: dash.Dash) -> None:
+    '''Check that a SortableItem with a handle (can) cannot be dragged if clicked (inside) outside of the handle.'''
+
+    dash_duo.start_server(app_with_two_items)
+    actions = ActionChains(dash_duo.driver)
+
+    source = dash_duo.find_element('component1', attribute='ID')
+    target = dash_duo.find_element('component2', attribute='ID')
+
+    initial_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    # Check that moving does not work when clicking outside of the handle
+    actions.click_and_hold(source)
+    actions.pause(0.5)
+    actions.move_to_element(target)
+    actions.pause(0.5)
+    actions.release().perform()
+
+    final_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    assert initial_locations == final_locations, 'Dragging and dropping should not work outside of handle when provided.'
+
+    # Check that dragging the handle does move the row as expected
+    handle = source.find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, 'label')
+    
+    actions.click_and_hold(handle)
+    actions.pause(0.5)
+    actions.move_to_element(target)
+    actions.pause(0.5)
+    actions.release().perform()
+
+    final_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+    
+    assert initial_locations != final_locations, 'Dragging and dropping component1 failed.'
+    assert final_locations['source']['y'] > initial_locations['source']['y'], 'source did not move vertically'
+    assert final_locations['target']['y'] < initial_locations['target']['y'], 'target did not move vertically'
+    assert final_locations['source']['y'] > final_locations['target']['y'], 'source is not below target after drag and drop'
+
+    return
+
+def test_moving_locked_item_without_handle(dash_duo: DashComposite, app_with_locked_items: dash.Dash) -> None:
+    '''Check that a SortableItem without a handle that is locked cannot be moved.'''
+
+    dash_duo.start_server(app_with_locked_items)
+    actions = ActionChains(dash_duo.driver)
+
+    source = dash_duo.find_element('component-locked1', attribute='ID')
+    target = dash_duo.find_element('component-free',    attribute='ID')
+
+    initial_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    # Check that dragging does not work
+    actions.click_and_hold(source)
+    actions.pause(0.5)
+    actions.move_to_element(target)
+    actions.pause(0.5)
+    actions.release().perform()
+
+    final_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    assert initial_locations == final_locations, 'Dragging should not work on locked items.'
+
+    return
+
+def test_moving_locked_item_with_handle(dash_duo: DashComposite, app_with_locked_items: dash.Dash) -> None:
+    '''Check that a SortableItem with a handle that is locked cannot be moved.'''
+
+    dash_duo.start_server(app_with_locked_items)
+    actions = ActionChains(dash_duo.driver)
+
+    source = dash_duo.find_element('component-locked2', attribute='ID')
+    target = dash_duo.find_element('component-free',    attribute='ID')
+
+    initial_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    handle = source.find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, 'label')
+
+    # Check that dragging does not work
+    actions.click_and_hold(handle)
+    actions.pause(0.5)
+    actions.move_to_element(target)
+    actions.pause(0.5)
+    actions.release().perform()
+
+    final_locations = {
+        'source' : source.location.copy(),
+        'target' : target.location.copy()
+    }
+
+    assert initial_locations == final_locations, 'Dragging the handle should not work on locked items.'
+
+    return
---
+++ test/python/test_rendering.py
@@ -0,0 +1,103 @@
+'''Integration tests that check that rendering of the SortableGroup and SortableItem components works as expected.'''
+
+import dash
+from   dash.testing.composite                import DashComposite
+from   selenium.webdriver.common.by          import By
+
+from   .fixtures import app_with_four_items
+
+def test_group_and_items_global_rendering(dash_duo: DashComposite, app_with_four_items: dash.Dash):
+    r'''Checks that a single group element and its item children are all rendered correctly.'''
+
+    dash_duo.start_server(app_with_four_items)
+
+    # Check that the group object is rendered
+    element = dash_duo.find_element('group', attribute='ID')
+    assert element.get_attribute('id') == 'group'
+
+    # Check that all items are rendered and are children of the group
+    rows = element.find_elements(By.CLASS_NAME, 'row')
+    assert len(rows) == 4, 'Wrong number of items'
+
+def test_order_of_items(dash_duo: DashComposite, app_with_four_items: dash.Dash):
+    r'''Checks that items are rendered at startup correctly based on their index value.'''
+
+    dash_duo.start_server(app_with_four_items)
+
+    element = dash_duo.find_element('group', attribute='ID')
+    rows    = element.find_elements(By.CLASS_NAME, 'row')
+
+    ids_in_order = [f'component{i}' for i in (2, 1, 3, 4)]
+
+    for pos, row in enumerate(rows):
+        assert row.get_attribute('id') == ids_in_order[pos], f'Row {pos} does not have the right ID.'
+
+def test_component1_children_rendering(dash_duo: DashComposite, app_with_four_items: dash.Dash):
+    '''Check that the children of the first component are rendered correctly.'''
+
+    dash_duo.start_server(app_with_four_items)
+
+    row = dash_duo.find_element('component1', attribute='ID')
+
+    # Check that the component does not have the dnd-kit button props
+    assert row.get_attribute('role') != 'button', 'component1 should not be draggable because it has a handle.'
+
+    children = row.find_elements(By.XPATH, "./child::*")
+
+    # Check that the item has two children, one div and one label
+    assert len(children) == 2, 'Wrong number of children found in component1.'
+
+    tags = ['div', 'label']
+    for tag, child in zip(tags, children):
+        assert child.tag_name == tag
+
+    # Check that the div has the right properties for a dnd-kit handle
+    assert children[0].get_attribute('role') == 'button', 'Wrong role for div child in component1.'
+
+    children_of_div = children[0].find_elements(By.XPATH, "./child::*")
+    assert len(children_of_div) == 1, 'Wrong number of children found for the handle.'
+    assert children_of_div[0].tag_name == 'label', 'Child of the div handle is not a label.'
+
+def test_component2_children_rendering(dash_duo: DashComposite, app_with_four_items: dash.Dash):
+    '''Check that the children of the second component are rendered correctly.'''
+
+    dash_duo.start_server(app_with_four_items)
+
+    row = dash_duo.find_element('component2', attribute='ID')
+
+    # Check that the component does have the dnd-kit button props but is disabled
+    assert row.get_attribute('role') == 'button', 'component2 should be draggable because it does not have a handle.'
+    assert row.get_attribute('aria-disabled') == 'true', 'component2 is enabled but it should be disabled.'
+
+    children = row.find_elements(By.XPATH, "./child::*")
+
+    # Check that the item has two children, one div and one label
+    assert len(children) == 2, 'Wrong number of children found in component2.'
+
+    tags = ['label', 'button']
+    for tag, child in zip(tags, children):
+        assert child.tag_name == tag
+
+def test_component3_children_rendering(dash_duo: DashComposite, app_with_four_items: dash.Dash):
+    '''Check that the children of the third component are rendered correctly.'''
+
+    dash_duo.start_server(app_with_four_items)
+
+    row = dash_duo.find_element('component3', attribute='ID')
+
+    # Check that the component does have the dnd-kit button props but is disabled
+    assert row.get_attribute('role') == 'button', 'component3 should be draggable because it does not have a handle.'
+    assert row.get_attribute('aria-disabled') == 'false', 'component2 is disabled but it should be enabled.'
+
+    children = row.find_elements(By.XPATH, "./child::*")
+
+    # Check that the item has two children, one div and one label
+    assert len(children) == 2, 'Wrong number of children found in component3.'
+
+    tags = ['div', 'label']
+    for tag, child in zip(tags, children):
+        assert child.tag_name == tag
+
+    children_of_div = children[0].find_elements(By.XPATH, "./child::*")
+    assert len(children_of_div) == 1, 'Wrong number of children found for the first child of component3.'
+    assert children_of_div[0].tag_name == 'input', 'Child of the div handle is not an input.'

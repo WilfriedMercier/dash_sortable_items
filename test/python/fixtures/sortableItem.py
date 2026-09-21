b60a0bb1a5aa8d32516a14f966fa945f7cb0e7d1
Wilfried <wifried.mercier.pro@pm.me>
2026-09-21 20:21:30 +0200

    Added a new test to check that styling is updated when dragging an element with a custom drag_styles props

--- test/python/fixtures/sortableItem.py
+++ test/python/fixtures/sortableItem.py
@@ -70,3 +70,41 @@ def app_button_no_handle__item() -> dash.Dash:
     app.layout = dash.html.Div([group, button], style={'display' : 'flex'})
 
     return app
+
+@pytest.fixture
+def app_drag_style__item() -> dash.Dash:
+    '''Fixture that creates a Dash app with items whose style changes when being dragged.'''
+
+    app = dash.Dash(__name__)
+
+    item1 = SortableItem(
+        id        = 'item1',
+        children  = [dash.html.Label('First row')],
+        handle    = dash.html.Label('☃'),
+        handlePos = 'start',
+        styles    = {
+            'handle' : {'backgroundColor' : 'red'},
+            'div'    : {'backgroundColor' : 'yellow'}
+        }, # type: ignore
+        styles_drag = {
+            'handle' : {'backgroundColor' : 'green'},
+            'div'    : {'backgroundColor' : 'blue', 'rotate' : '180deg'}
+        }, # type: ignore
+        className = 'row'
+    )
+    
+    item2 = SortableItem(
+        id        = 'item2',
+        children  = [dash.html.Label('Second row')],
+        className = 'row',
+    )
+
+    group = SortableGroup(
+        id        = 'group',
+        className = 'group',
+        items     = [item1, item2]
+    )
+
+    app.layout = dash.html.Div(group, style={'display' : 'flex'})
+
+    return app
